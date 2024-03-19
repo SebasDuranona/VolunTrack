e08af4eae4b81334665b83367379c351a24f9cb6
Kyle Harrison <kmh1659@gmail.com>
2024-03-18 20:57:36 -0500

    initial spring commit

---
+++ src/main/java/org/voluntrack/voluntrack/VoluntrackApplication.java
@@ -0,0 +1,13 @@
+package org.voluntrack.voluntrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoluntrackApplication.class, args);
+    }
+
+}
